c5be86d783aa880fbccbc310dc22477cb6131c0f
Thiago-S-Sousa <Thiago_S_Sousa@outlook.com>
2025-09-09 10:46:56 -0300

    feat: desktop app start - initial commit

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const currentYear = new Date().getFullYear();
+const owner = "Bárbara Medeiros";
+const text = `© ${currentYear} ${owner}.  Todos os direitos reservados.`;
+document.getElementById("footer-text").textContent = text;
+
+// ------ FOOTER ------ //
